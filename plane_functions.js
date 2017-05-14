2bff11b8ed0c67bfa8753a4738d1546944d9960c
Ajmal Moochingal <moochingal@localhost.localdomain>
2017-05-14 10:26:02 +0530

    ui-update

--- plane_functions.js
+++ plane_functions.js
@@ -1,3 +1,4 @@
+var invalid = "Invalid input";
 
 function formsubmit(formNum){
   switch (formNum) {
@@ -6,7 +7,14 @@ function formsubmit(formNum){
       $(function(){
         action =  parseFloat($('#param1').val()) + parseFloat($('#param2').val()) + parseFloat($('#param3').val());
         console.log(action);
-        $('#outDisp').text(action);
+
+        if(action>0 | action<0 | action==0){
+            $('#outDisp').text(action);
+        }
+        else {
+            $('#outDisp').text(invalid);
+        }
+
       });
       break;
     default:
